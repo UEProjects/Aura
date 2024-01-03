e573e357962cec1266ba3cfaa2b0d297c9312279
Sixto <6togomez@gmail.com>
2024-01-02 22:23:39 -0500

    Set up Idle Animations and Input Actions
    - Setup Blueprints for Aura, Goblin_Slingshot and Goblin_Spear enemies.
    - Setup Animation Blueprint for Aura, Goblin_Slingshot and Goblin_Spear.
    - Created Input Action 'IA_Move' and the InputMappingContext 'IMC_AuraContext'.
    - Created 'AuraPlayerController' class to handle Input Actions for movement.

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,31 @@
+// Copyright by Sixto.
+
+
+#include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+
+	// EnhancedInput settings subsystem...
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	// Mouse cursor settings...
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
