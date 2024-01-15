69e3722b0d0e76ed22f2b49a59e99d9c2ec414c2
Sixto <6togomez@gmail.com>
2024-01-14 20:28:05 -0500

    Created UserWidget class and the WBP_GlobeProgressBar
    - Added the 'AuraUserWidget' class and 'AuraWidgetController' class.
    - Added Widget Blueprint to use for the progress bars.

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright by Sixto.
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright by Sixto.
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
